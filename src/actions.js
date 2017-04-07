7428a16d0664bb1f177a2c7afc36e1eb502792d5
Baron Willeford <baron.willeford@gmail.com>
2017-04-07 18:34:33 -0400

    adds actions for resetting state and fetching playlist data

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,34 @@
 import {
-  SET_ACCESS_TOKEN
+  SET_ACCESS_TOKEN,
+  FETCH_PLAYLISTS,
+  RESET_STATE,
+  FETCH_PLAYLIST_ITEMS
 } from './actionCreators';
 
+//Redux
 export function setAccessToken(token) {
   return {
     type: SET_ACCESS_TOKEN,
     token
+  };
+}
+
+export function resetState() {
+  return {
+    type: RESET_STATE
+  };
+}
+
+//Epic
+export function fetchPlaylists() {
+  return {
+    type: FETCH_PLAYLISTS
+  };
+}
+
+export function fetchPlaylistItems(playlistId) {
+  return {
+    type: FETCH_PLAYLIST_ITEMS,
+    playlistId
   };
 }
