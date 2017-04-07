6123bb24dd82998669eb4d63413c79dac439f334
Baron Willeford <baron.willeford@gmail.com>
2017-04-07 18:33:37 -0400

    reducer can reset itself, include playlists and playlist items

--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,42 @@
-import { fromJS } from 'immutable';
+import { fromJS, List } from 'immutable';
 
 import {
-  SET_ACCESS_TOKEN
+  SET_ACCESS_TOKEN,
+  GET_PLAYLISTS,
+  RESET_STATE,
+  GET_PLAYLIST_ITEMS
 } from './actionCreators';
 
 // import { REHYDRATE } from 'redux-persist/constants';
 
 const initialState = fromJS({
-  accessToken: ''
+  accessToken: '',
+  playlists: [],
+  playlistItems: []
 });
 
 function setToken(state, token) {
-
   return state.set('accessToken', token);
+}
+
+function setPlaylists(state, payload) {
+  return state.updateIn(['playlists'], (list) => list.concat(payload.items));
+}
+
+function setPlaylistItems(state, payload) {
+  return state.set('playlistItems', List(payload.items));
 }
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case RESET_STATE:
+      return initialState;
     case SET_ACCESS_TOKEN:
       return setToken(state, action.token);
+    case GET_PLAYLISTS:
+      return setPlaylists(state, action.payload);
+    case GET_PLAYLIST_ITEMS:
+      return setPlaylistItems(state, action.payload);
     default:
       return state;
   }
