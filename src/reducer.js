11f704643727c0f98e0be01c7514797019117569
Baron Willeford <baron.willeford@gmail.com>
2017-04-15 13:57:12 -0400

    adds lots of reducer functionality and tests

--- src/reducer.js
+++ src/reducer.js
@@ -6,7 +6,12 @@ import {
   RESET_STATE,
   GET_PLAYLIST_ITEMS,
   NEXT_VIDEO,
-  GOTO_VIDEO
+  GOTO_VIDEO,
+  CHANGE_PLAY_STATE,
+  INCREMENT_TIME,
+  RESET_CLOCK,
+  CHANGE_VIDEO_LENGTH,
+  FLIP_NEXT
 } from './actionCreators';
 
 // import { REHYDRATE } from 'redux-persist/constants';
@@ -15,7 +20,11 @@ const initialState = fromJS({
   accessToken: '',
   playlists: [],
   playlistItems: [],
-  playlistIndex: 0
+  playlistIndex: 0,
+  playing: false,
+  time: 0,
+  videoLength: 60,
+  callNext: true
 });
 
 function setToken(state, token) {
@@ -41,6 +50,29 @@ function setVideoIndex(state, index) {
   return state.set('playlistIndex', Number(index));
 }
 
+function changePlayState(state, bool) {
+  return state.set('playing', bool);
+}
+
+function incrementTime(state) {
+  if (state.get('playing')) {
+    return state.set('time', state.get('time') + 1);
+  }
+  return state;
+}
+
+function resetClock(state) {
+  return state.set('time', 0);
+}
+
+function changeVideoLength(state, length) {
+  return state.set('videoLength', Number(length));
+}
+
+function flipNext(state) {
+  return state.set('callNext', !state.get('callNext'));
+}
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case RESET_STATE:
@@ -55,6 +87,16 @@ export default function reducer(state = initialState, action) {
       return incrementPlaylistIndex(state);
     case GOTO_VIDEO:
       return setVideoIndex(state, action.index);
+    case CHANGE_PLAY_STATE:
+      return changePlayState(state, action.bool);
+    case INCREMENT_TIME:
+      return incrementTime(state);
+    case RESET_CLOCK:
+      return resetClock(state);
+    case CHANGE_VIDEO_LENGTH:
+      return changeVideoLength(state, action.length);
+    case FLIP_NEXT:
+      return flipNext(state);
     default:
       return state;
   }
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,8 +4,13 @@ import {
   SET_ACCESS_TOKEN,
   GET_PLAYLISTS,
   GET_PLAYLIST_ITEMS,
+  NEXT_VIDEO,
   GOTO_VIDEO,
-  NEXT_VIDEO
+  CHANGE_PLAY_STATE,
+  INCREMENT_TIME,
+  RESET_CLOCK,
+  CHANGE_VIDEO_LENGTH,
+  FLIP_NEXT
 } from './actionCreators';
 
 import reducer from './reducer';
@@ -140,6 +145,103 @@ describe('reducer', function () {
       expect(nextState).toEqual(Map({
         playlistIndex: 2,
         playlistItems: List([Map({id: 'asdgew'}), Map({id: '32srfrg'}), Map({id: 'ydfbsbm2'})])
+      }));
+    });
+  });
+
+  describe('CHANGE_PLAY_STATE', function () {
+    it('switches the play state', function () {
+      const initialState = fromJS({
+        playing: true
+      });
+
+      const action = {
+        type: CHANGE_PLAY_STATE,
+        bool: false
+      };
+
+
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).toEqual(Map({
+        playing: false
+      }));
+    });
+  });
+
+  describe('INCREMENT_TIME', function () {
+    it('increments the clock when playing', function () {
+      const initialState = fromJS({
+        playing: true,
+        time: 8
+      });
+
+      const nextState = reducer(initialState, { type: INCREMENT_TIME });
+
+      expect(nextState).toEqual(Map({
+        playing: true,
+        time: 9
+      }));
+    });
+
+    it('doesnt increment the clock when it is not playing', function () {
+      const initialState = fromJS({
+        playing: false,
+        time: 8
+      });
+
+      const nextState = reducer(initialState, { type: INCREMENT_TIME });
+
+      expect(nextState).toEqual(Map({
+        playing: false,
+        time: 8
+      }));
+    });
+  });
+
+  describe('RESET_CLOCK', function () {
+    it('resets the clock time to zero', function () {
+      const initialState = fromJS({
+        time: 45
+      });
+
+      const nextState = reducer(initialState, { type: RESET_CLOCK });
+
+      expect(nextState).toEqual(Map({
+        time: 0
+      }));
+    });
+  });
+
+  describe('CHANGE_VIDEO_LENGTH', function () {
+    it('sets the video length', function () {
+      const initialState = fromJS({
+        videoLength: 60
+      });
+
+      const action = {
+        type: CHANGE_VIDEO_LENGTH,
+        length: 80
+      };
+
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).toEqual(Map({
+        videoLength: 80
+      }));
+    });
+  });
+
+  describe('FLIP_NEXT', function () {
+    it('reverses callNext', function () {
+      const initialState = fromJS({
+        callNext: false
+      });
+
+      const nextState = reducer(initialState, { type: FLIP_NEXT });
+
+      expect(nextState).toEqual(Map({
+        callNext: true
       }));
     });
   });
