108418ef5e96a35f14a0be51f57da0c35d9833cf
Baron Willeford <baron.willeford@gmail.com>
2017-04-15 13:25:55 -0400

    large refactor

--- src/App.js
+++ src/App.js
@@ -1,94 +1,60 @@
 import React from 'react';
 import './App.css';
-import Login from './Login/Login';
 import { connect } from 'react-redux';
-import YouTube from 'react-youtube';
 import { persistor } from './store';
 
-import { fetchPlaylists, resetState, fetchPlaylistItems, nextVideo, goToVideo } from './actions';
+import Login from './Login/Login';
+import Clock from './Clock';
+import Playlists from './Playlists';
+import PlaylistItems from './PlaylistItems';
+import Video from './Video';
 
-function App(params) {
-  const c = new React.Component(params);
+import {
+  fetchPlaylists,
+  resetState,
+  changeVideoLength,
+  incrementTime,
+} from './actions';
+
+function App(props) {
+  const c = new React.Component(props);
 
   c.state = {
-    callNext: true
+    intervalId: 0
   };
 
-  c.render = function() {
+  c.componentDidMount = function () {
+    const intervalId = setInterval(() => c.props.moveClock(), 1000);
+    c.setState({intervalId});
+  };
 
+  c.componentWillUnmount = function () {
+    clearInterval(c.state.intervalId);
+  };
+
+  c.render = function () {
     return (
       <div className="App">
 
-        <button onClick={getPlaylists}>Click Me</button>
+      <button onClick={c.props.getPlaylists}>Click Me</button>
 
-        {!c.props.accessToken.length ?
-          <Login /> : <button onClick={logout}>Logout</button>
-        }
+      {!c.props.accessToken.length ?
+        <Login /> : <button onClick={c.props.logout}>Logout</button>
+      }
 
-        <div className="Playlists">
-          <ul>
-            {c.props.playlists.map(function (list) {
-              return(
-                <li key={list.id} onClick={getPlaylist(list.id)}>{list.snippet.title}</li>
-              );
-            })}
-          </ul>
-        </div>
+      <Playlists />
 
-        <div className="PlaylistItems">
-          <ul>
-            {c.props.playlistItems.map(function (item, index) {
-              return(
-                <li key={item.id} data-index={index} onClick={setVideoIndex}>{item.snippet.title}</li>
-              );
-            })}
-          </ul>
-        </div>
+      <PlaylistItems />
 
-        <div className="currentVideo">
-          {c.props.playlistItems.size &&
-            <YouTube
-              videoId={videoId()}
-              opts={{playerVars: {start: 30, end: 40, autoplay: 1, rel: 0}}}
-              onEnd={handleVideoEnd}
-            />
-          }
-        </div>
+
+      <Clock />
+
+      <input type="number" min={10} max={120} step={10} onChange={c.props.changeVidLen} value={c.props.videoLength}/>
+
+      <Video />
       </div>
     );
   };
-
-  function getPlaylist(playlistId) {
-    return function () {
-      c.props.dispatch(fetchPlaylistItems(playlistId));
-    };
-  }
-
-  function setVideoIndex(event) {
-    c.props.dispatch(goToVideo(event.target.dataset.index));
-  }
-
-  function videoId() {
-    const { playlistItems, playlistIndex } = c.props;
-    return playlistItems.get(playlistIndex).snippet.resourceId.videoId;
-  }
-
-  function handleVideoEnd() {
-    // if statement is workaround for bug that causes onEnd to be called twice
-    if (c.state.callNext) {
-      c.props.dispatch(nextVideo());
-    }
-    c.setState({callNext: !c.state.callNext});
-  }
-
-  function logout() {
-    persistor.purge();
-    c.props.dispatch(resetState());
-  }
-
-  function getPlaylists() {
-    c.props.dispatch(fetchPlaylists());
-  }
 
   return c;
 }
@@ -96,10 +62,35 @@ function App(params) {
 function mapStateToProps(state) {
   return {
     accessToken: state.get('accessToken'),
-    playlists: state.get('playlists'),
-    playlistItems: state.get('playlistItems'),
-    playlistIndex: state.get('playlistIndex')
+    videoLength: state.get('videoLength'),
   };
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  function changeVidLen(event) {
+    dispatch(changeVideoLength(event.target.value));
+  }
+
+  function getPlaylists() {
+    dispatch(fetchPlaylists());
+  }
+
+  function logout() {
+    persistor.purge();
+    dispatch(resetState());
+  }
+
+
+  function moveClock() {
+    dispatch(incrementTime());
+  }
+
+  return {
+    changeVidLen,
+    getPlaylists,
+    logout,
+    moveClock
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
