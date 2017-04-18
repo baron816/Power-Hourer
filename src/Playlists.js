e4e019876a468c24a3ab7e06e10d5b646106c300
Baron Willeford <baron.willeford@gmail.com>
2017-04-18 19:01:27 -0400

    parses playlist item from immutable correctly

--- src/Playlists.js
+++ src/Playlists.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { List, ListItem } from 'material-ui/List';
 import Paper from 'material-ui/Paper';
-import './Playlists.css'
+import './Playlists.css';
 
-import { fetchPlaylistItems, goToVideo, changePlayState, resetClock } from './actions';
+import {
+  fetchPlaylistItems,
+  goToVideo,
+  changePlayState,
+  resetClock,
+  invertModalState,
+  setPlaylistName
+} from './actions';
 
 function Playlists({playlists, getPlaylists}) {
   return (
     <Paper zDepth={3} id="playlists">
       <List>
         {playlists.map(function (list) {
+          const id = list.get('id');
+          const title = list.getIn(['snippet', 'title']);
           return(
-            <ListItem key={list.id} onClick={getPlaylists(list.id)}>{list.snippet.title}</ListItem>
+            <ListItem key={id} onClick={getPlaylists(id, title)}>{title}</ListItem>
           );
         })}
       </List>
@@ -27,12 +36,14 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  function getPlaylists(listId) {
+  function getPlaylists(listId, title) {
     return function () {
+      dispatch(setPlaylistName(title));
       dispatch(fetchPlaylistItems(listId));
       dispatch(goToVideo(0));
       dispatch(changePlayState(false));
       dispatch(resetClock());
+      dispatch(invertModalState());
     };
   }
 
