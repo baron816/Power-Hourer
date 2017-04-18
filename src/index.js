3d54e04c78fc6db5e92f067803b61473bf138516
Baron Willeford <baron.willeford@gmail.com>
2017-04-18 17:14:54 -0400

    injects tap event plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './App';
 import store from './store';
 import './index.css';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <Provider store={store}>
