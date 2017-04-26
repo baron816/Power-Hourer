c90048f68338ac61f3221be668ca1df5aaf1518d
Baron Willeford <baron.willeford@gmail.com>
2017-04-26 17:39:43 -0500

    adds material styling and gets playlists on login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton'
+import MusicNote from 'material-ui/svg-icons/image/music-note';
 
 import { persistor } from './store';
 import Login from './Login/Login';
@@ -10,21 +13,20 @@ import Playlists from './Playlists';
 import VideoModal from './VideoModal';
 
 import {
-  fetchPlaylists,
+
   resetState,
   changeVideoLength
 } from './actions';
 
-function App({accessToken, videoLength, getPlaylists, logout, changeVidLen}) {
+function App({accessToken, videoLength, logout, changeVidLen}) {
   return (
     <MuiThemeProvider>
       <div className="App">
-        <AppBar title="Power Hourer" />
-        <button onClick={getPlaylists}>Click Me</button>
-
-        {!accessToken.length ?
-          <Login /> : <button onClick={logout}>Logout</button>
-        }
+        <AppBar
+          title="Power Hourer"
+          iconElementRight={accessToken.length ? <FlatButton onClick={logout} label="Logout" /> : <Login />}
+          iconElementLeft={<IconButton><MusicNote /></IconButton>}
+          />
 
         <Playlists />
 
@@ -47,9 +49,7 @@ function mapDispatchToProps(dispatch) {
     dispatch(changeVideoLength(event.target.value));
   }
 
-  function getPlaylists() {
-    dispatch(fetchPlaylists());
-  }
+
 
   function logout() {
     persistor.purge();
@@ -58,7 +58,6 @@ function mapDispatchToProps(dispatch) {
 
   return {
     changeVidLen,
-    getPlaylists,
     logout
   };
 }
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,29 +1,34 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { connect } from 'react-redux';
-import { setAccessToken } from '../actions';
+import { setAccessToken, fetchPlaylists } from '../actions';
 
-function Login(props) {
-  const c = new React.Component(props);
-
-  c.render = function () {
-    return (
-      <GoogleLogin
-      clientId="727985332451-et0hhrtnpccevvstmjm1e11vv7kc16pd.apps.googleusercontent.com"
-      buttonText="Login"
-      discoveryDocs="https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
-      scope="https://www.googleapis.com/auth/youtube.readonly"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      />
-    );
-  };
-
-  function responseGoogle(response) {
-    c.props.dispatch(setAccessToken(response.accessToken));
-  }
-
-  return c;
+function Login({setToken}) {
+  return (
+    <GoogleLogin
+    clientId="727985332451-et0hhrtnpccevvstmjm1e11vv7kc16pd.apps.googleusercontent.com"
+    buttonText="Login"
+    discoveryDocs="https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+    scope="https://www.googleapis.com/auth/youtube.readonly"
+    onSuccess={setToken}
+    onFailure={setToken}
+    />
+  );
 }
 
-export default connect()(Login);
+function mapStateToProps() {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  function setToken({accessToken}) {
+    dispatch(setAccessToken(accessToken));
+    dispatch(fetchPlaylists());
+  }
+
+  return {
+    setToken
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
