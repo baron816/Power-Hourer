bd7ead5658190edc4c659064284c29d21cf1623f
Baron Willeford <baron.willeford@gmail.com>
2017-05-03 12:51:09 -0500

    signIn instead of setToken

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,15 +3,15 @@ import GoogleLogin from 'react-google-login';
 import { connect } from 'react-redux';
 import { setAccessToken, fetchPlaylists } from '../actions';
 
-function Login({setToken}) {
+function Login({signIn}) {
   return (
     <GoogleLogin
-    clientId="727985332451-et0hhrtnpccevvstmjm1e11vv7kc16pd.apps.googleusercontent.com"
-    buttonText="Login"
-    discoveryDocs="https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
-    scope="https://www.googleapis.com/auth/youtube.readonly"
-    onSuccess={setToken}
-    onFailure={setToken}
+      clientId="727985332451-et0hhrtnpccevvstmjm1e11vv7kc16pd.apps.googleusercontent.com"
+      buttonText="Login"
+      discoveryDocs="https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+      scope="https://www.googleapis.com/auth/youtube.readonly"
+      onSuccess={signIn}
+      onFailure={signIn}
     />
   );
 }
@@ -21,13 +21,13 @@ function mapStateToProps() {
 }
 
 function mapDispatchToProps(dispatch) {
-  function setToken({accessToken}) {
+  function signIn({accessToken}) {
     dispatch(setAccessToken(accessToken));
     dispatch(fetchPlaylists());
   }
 
   return {
-    setToken
+    signIn
   };
 }
 
