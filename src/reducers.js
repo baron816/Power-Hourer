53cd4dda1d38fa7daba45fbe485b2db1f30778b3
Baron Willeford <baron.willeford@gmail.com>
2017-05-03 18:13:03 -0500

    adds createUser reducer

--- src/reducers.js
+++ src/reducers.js
@@ -4,10 +4,12 @@ import root from './rootReducer';
 import playlistItems from './PlaylistItems/playlistItemsReducer';
 import playlists from './Playlists/playlistsReducer';
 import clock from './Clock/clockReducer';
+import createUser from './CreateUser/createUserReducer';
 
 export default combineReducers({
   root,
   playlistItems,
   playlists,
-  clock
+  clock,
+  createUser
 });
