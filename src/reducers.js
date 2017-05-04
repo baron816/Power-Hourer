a26a88dc975e74c014373b0e001bca3bc8e3cdf8
Baron Willeford <baron.willeford@gmail.com>
2017-05-04 15:05:04 -0500

    fully resets state on logout

--- src/reducers.js
+++ src/reducers.js
@@ -5,11 +5,20 @@ import playlistItems from './PlaylistItems/playlistItemsReducer';
 import playlists from './Playlists/playlistsReducer';
 import clock from './Clock/clockReducer';
 import createUser from './CreateUser/createUserReducer';
+import { RESET_STATE } from './actionCreators';
 
-export default combineReducers({
+const appReducer = combineReducers({
   root,
   playlistItems,
   playlists,
   clock,
   createUser
 });
+
+export default function reducers(state, action) {
+  if (action.type === RESET_STATE) {
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+}
