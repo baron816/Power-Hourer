656b911f07100c4ef84cccef109fad70fddee7ca
Baron Willeford <baron.willeford@gmail.com>
2017-05-08 10:43:57 -0500

    handles setting serverPlaylists

--- src/Playlists/playlistsReducer.js
+++ src/Playlists/playlistsReducer.js
@@ -1,26 +1,34 @@
 import { fromJS } from 'immutable';
 
-import { GET_PLAYLISTS, SET_PLAYLIST_INDEX } from '../actionCreators';
+import { GET_PLAYLISTS, SET_PLAYLIST_INDEX, SET_SERVER_PLAYLISTS } from '../actionCreators';
 
 const initialState = fromJS({
     playlists: [],
+    serverPlaylists: [],
     playlistIndex: 0
 });
-
-function setPlaylists(state, payload) {
-  return state.updateIn(['playlists'], (list) => list.concat(fromJS(payload)));
-}
 
 function setPlaylistIndex(state, index) {
   return state.set('playlistIndex', index);
 }
 
+function playlistSetter(type) {
+  return function (state, playlists) {
+    return state.updateIn([type], (list) => list.concat(fromJS(playlists)));
+  };
+}
+
+const setServerPlaylists = playlistSetter('serverPlaylists');
+const setYouTubePlaylists = playlistSetter('playlists');
+
 export default function playlistsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_PLAYLISTS:
-      return setPlaylists(state, action.payload);
+      return setYouTubePlaylists(state, action.payload);
     case SET_PLAYLIST_INDEX:
       return setPlaylistIndex(state, action.index);
+    case SET_SERVER_PLAYLISTS:
+      return setServerPlaylists(state, action.playlists);
     default:
       return state;
   }
--- src/Playlists/playlistsReducer.test.js
+++ src/Playlists/playlistsReducer.test.js
@@ -1,12 +1,11 @@
 import { fromJS, List, Map } from 'immutable';
 
 import reducer from './playlistsReducer';
-import { GET_PLAYLISTS, SET_PLAYLIST_INDEX } from '../actionCreators';
+import { GET_PLAYLISTS, SET_PLAYLIST_INDEX, SET_SERVER_PLAYLISTS } from '../actionCreators';
 
 describe('playlistsReducer', function () {
   describe('GET_PLAYLISTS', function () {
     const initialState = fromJS({
-      accessToken: '',
       playlists: []
     });
 
@@ -19,7 +18,6 @@ describe('playlistsReducer', function () {
       const nextState = reducer(initialState, action);
 
       expect(nextState).toEqual(Map({
-        accessToken: '',
         playlists: List([Map({name: 'Cheezy-Tunez'}), Map({name: 'Hot Girl'})])
       }));
     });
@@ -40,6 +38,25 @@ describe('playlistsReducer', function () {
 
       expect(nextState).toEqual(Map({
         playlistIndex: 4
+      }));
+    });
+  });
+
+  describe('SET_SERVER_PLAYLISTS', function () {
+    it('sets the state with playlists from the server', function () {
+      const initialState = fromJS({
+        serverPlaylists: []
+      });
+
+      const action = {
+        type: SET_SERVER_PLAYLISTS,
+        playlists: [{name: 'TSwift/Jason Durulo'}, {name: 'Jock Jams'}]
+      };
+
+      const nextState = reducer(initialState, action);
+
+      expect(nextState).toEqual(Map({
+        serverPlaylists: List([Map({name: 'TSwift/Jason Durulo'}), Map({name: 'Jock Jams'})])
       }));
     });
   });
