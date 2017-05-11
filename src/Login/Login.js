62695c46e51f5839b314f8e81123ecd11d97a47e
Baron Willeford <baron.willeford@gmail.com>
2017-05-11 18:57:20 -0500

    will dispatch getServerPlaylists

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { connect } from 'react-redux';
-import { setAccessToken, fetchPlaylists } from '../actions';
+import { setAccessToken, fetchPlaylists, getServerPlaylists } from '../actions';
 
 function Login({signIn}) {
   return (
@@ -24,6 +24,7 @@ function mapDispatchToProps(dispatch) {
   function signIn({accessToken, googleId}) {
     dispatch(setAccessToken(accessToken, googleId));
     dispatch(fetchPlaylists());
+    dispatch(getServerPlaylists());
   }
 
   return {
