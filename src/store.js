65b2528b625c36502b9a9629d2e2ddaae28d8f9a
Baron Willeford <baron.willeford@gmail.com>
2017-06-22 20:49:24 -0500

    removes loging and uses composeEnhancers

--- src/store.js
+++ src/store.js
@@ -7,23 +7,11 @@ import epic from './epics';
 
 const epicMiddleWare = createEpicMiddleware(epic);
 
-function addLoggingToDispatch(store) {
-  if (false) {
-    const rawDispatch = store.dispatch;
-    return function (action) {
-      console.group(action.type);
-      console.log('%c prev state', 'color: red', store.getState().toJS());
-      console.log('%c action', 'color: blue', action);
-      const returnValue = rawDispatch(action);
-      console.log('%c next state', 'color: green', store.getState().toJS());
-      console.groupEnd(action.type);
-      return returnValue;
-    };
-  }
-  return store.dispatch;
-}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), compose(applyMiddleware(epicMiddleWare), autoRehydrate()));
-store.dispatch = addLoggingToDispatch(store);
+const store = createStore(reducer, composeEnhancers(
+  applyMiddleware(epicMiddleWare),
+  autoRehydrate())
+);
 
 export default store;
