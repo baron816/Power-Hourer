d6abf185badbeb69110b17da37c0a158ff1ca5f7
Baron Willeford <baron.willeford@gmail.com>
2017-09-04 17:13:06 -0500

    removes log statement

--- src/PlaylistItems/playlistItemsEpics.js
+++ src/PlaylistItems/playlistItemsEpics.js
@@ -43,8 +43,6 @@ function updateVideo(store, payload) {
 
   const updateData = JSON.stringify({startTime: payload});
 
-  console.log(item)
-
   return ajax.put(`${SERVER_URL}playlists/${playlist}/playlistItems/${item}`, updateData, {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + token
